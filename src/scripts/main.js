ac2a4dac9b1b315b8f1aaf126bdab268eda7a6f5
Richard Gill <richard@rgill.co.uk>
2015-10-28 17:58:43 +0000

    Made bootstrap load via webpack instead of from a CDN

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,4 @@
 // This puts a <style> tag in <head> of page with transpiled css.
 require('../styles/main.scss')
+// Import bootstrap (which requires jQuery)
+require('imports?jQuery=jquery!bootstrap-sass')
--- webpack.config.js
+++ webpack.config.js
@@ -27,5 +27,4 @@ module.exports = {
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   }
-
 };
