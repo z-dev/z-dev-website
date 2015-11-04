d6b9471a5c3b57d54d481580a9a507916cd3c4e3
john <johnagill95@gmil.com>
2015-11-04 11:27:36 +0000

    removed colsole.log

--- src/scripts/dropdowntoggle.js
+++ src/scripts/dropdowntoggle.js
@@ -3,7 +3,6 @@ var $ = require('jquery')
 var dropdownToggleSetup = function() {
   $(document).ready(function() {
     $('#nav-bar').find('li').click(function() {
-      console.log('helo')
       $('#nav-bar').removeClass('in')
     });
   });
