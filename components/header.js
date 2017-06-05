3fd5a45496accef89d5c1dad1e742f26d3234c55
Alex Smith <altaudiolabs@gmail.com>
2017-06-05 10:04:56 +0100

    Addresses PR comments

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
     this.state = { collapsed: true }
   }
 
-  onClick(e) {
+  onResponsiveMenuClick() {
       this.setState({ collapsed: true })
   }
 
@@ -29,14 +29,14 @@ export default class Header extends Component {
                     <a className="brand" rel="home" data-scroll="true" href="#home" title="ZDev"><img src="/static/logo.svg" alt="ZDev logo" /></a>
                   </div> {/* /navbar-header */}
                   <div id="nav-bar" className={`${this.state.collapsed ? 'collapse' : ''} navbar-collapse`}>
-                    <ul className="nav navbar-nav navbar-right navbar-links">
-                      <li><a data-scroll="true" href="#about" onClick={this.onClick.bind(this)}>About</a></li>
-                      <li><a data-scroll="true" href="#services" onClick={this.onClick.bind(this)}>Services</a></li>
-                      <li><a data-scroll="true" href="#our-work" onClick={this.onClick.bind(this)}>Our Work</a></li>
-                      <li><a data-scroll="true" href="#techstack" onClick={this.onClick.bind(this)}>Tech</a></li>
-                      <li><a data-scroll="true" href="#team" onClick={this.onClick.bind(this)}>Team</a></li>
-                      <li><a href="https://blog.z-dev.io/" onClick={this.onClick.bind(this)}>Blog</a></li>
-                      <li><a data-scroll="true" href="#contact" onClick={this.onClick.bind(this)}>Contact</a></li>
+                    <ul className="nav navbar-nav navbar-right navbar-links" onClick={() => this.onResponsiveMenuClick()}>
+                      <li><a data-scroll="true" href="#about">About</a></li>
+                      <li><a data-scroll="true" href="#services">Services</a></li>
+                      <li><a data-scroll="true" href="#our-work">Our Work</a></li>
+                      <li><a data-scroll="true" href="#techstack">Tech</a></li>
+                      <li><a data-scroll="true" href="#team">Team</a></li>
+                      <li><a href="https://blog.z-dev.io/">Blog</a></li>
+                      <li><a data-scroll="true" href="#contact">Contact</a></li>
                       <li />
                       <li />
                     </ul>
