979c8b4298d7ba1d41ded4505b230f96381ee962
Alex Smith <altaudiolabs@gmail.com>
2017-06-05 09:33:34 +0100

    Adds onClick function to nav links to close navigation in sm and xs modes

--- components/header.js
+++ components/header.js
@@ -25,13 +25,13 @@ export default class Header extends Component {
                   </div> {/* /navbar-header */}
                   <div id="nav-bar" className={`${this.state.collapsed ? 'collapse' : ''} navbar-collapse`}>
                     <ul className="nav navbar-nav navbar-right navbar-links">
-                      <li><a data-scroll="true" href="#about">About</a></li>
-                      <li><a data-scroll="true" href="#services">Services</a></li>
-                      <li><a data-scroll="true" href="#our-work">Our Work</a></li>
-                      <li><a data-scroll="true" href="#techstack">Tech</a></li>
-                      <li><a data-scroll="true" href="#team">Team</a></li>
-                      <li><a href="https://blog.z-dev.io/">Blog</a></li>
-                      <li><a data-scroll="true" href="#contact">Contact</a></li>
+                      <li><a data-scroll="true" href="#about" onClick={() => this.setState({ collapsed: true })}>About</a></li>
+                      <li><a data-scroll="true" href="#services" onClick={() => this.setState({ collapsed: true })}>Services</a></li>
+                      <li><a data-scroll="true" href="#our-work" onClick={() => this.setState({ collapsed: true })}>Our Work</a></li>
+                      <li><a data-scroll="true" href="#techstack" onClick={() => this.setState({ collapsed: true })}>Tech</a></li>
+                      <li><a data-scroll="true" href="#team" onClick={() => this.setState({ collapsed: true })}>Team</a></li>
+                      <li><a href="https://blog.z-dev.io/" onClick={() => this.setState({ collapsed: true })}>Blog</a></li>
+                      <li><a data-scroll="true" href="#contact" onClick={() => this.setState({ collapsed: true })}>Contact</a></li>
                       <li />
                       <li />
                     </ul>
