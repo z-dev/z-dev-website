7e3a0c7bb0f7cb6118d9f49415b32b33ed51dba6
Richard Gill <richard@rgill.co.uk>
2017-07-12 11:44:54 +0100

    Adds production and development config

--- config/index.js
+++ config/index.js
@@ -1,10 +1,24 @@
-export default {
-  firebaseConfig : {
-    apiKey: "AIzaSyB96CHqxhLWfb2jWnGU2duVSfgqi8m9h1Q",
-    authDomain: "z-dev-site-dev.firebaseapp.com",
-    databaseURL: "https://z-dev-site-dev.firebaseio.com",
-    projectId: "z-dev-site-dev",
-    storageBucket: "z-dev-site-dev.appspot.com",
-    messagingSenderId: "986603931876"
+/* eslint-disable*/
+import _ from 'lodash'
+import productionConfig from './production'
+import developmentConfig from './development'
+import defaults from './defaults'
+
+const environment = process.env.CONFIG_ENV
+
+const getEnvironment = () => {
+  switch (environment) {
+    case 'production':
+      return productionConfig
+    case 'development':
+      return developmentConfig
+    default:
+      return developmentConfig
   }
 }
+
+const config = _.merge({}, defaults, getEnvironment())
+
+console.log(`config: ${JSON.stringify(config)}`)
+
+export default config
