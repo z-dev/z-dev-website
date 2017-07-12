fbc6ff0af54c0914a9e27a443db4cd45f74f6a0c
Richard Gill <richard@rgill.co.uk>
2017-07-12 16:49:45 +0100

    Removes percentage widths and goes full flex-box

--- components/projectQueryForm.js
+++ components/projectQueryForm.js
@@ -1,43 +1,47 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
-import moment from 'moment-timezone'
+import moment from 'moment'
 import _ from 'lodash'
 import 'common/initializeFirebase.js'
 import Div from 'components/core/div.js'
 import Form from 'components/core/form.js'
 import Button from 'components/core/button.js'
 import ResponsiveContainer from 'components/core/responsiveContainer'
-import { H3 } from 'components/core/headerText'
+import { H3, H5 } from 'components/core/headerText'
 import styled from 'styled-components'
 
 const OuterContainer = styled(ResponsiveContainer)`
   #innerContainer {
-    align-items: center;
+    align-items: stretch;
+    justify-content: stretch;
+    flex: 1;
     border: solid 1px #dedee0;
     background: white;
     display: flex;
 
-    @media (min-width: ${props => props.theme.screenLgMin}px) {
+    @media (min-width: ${props => props.theme.screenSmMin}px) {
       max-width: ${props => props.theme.screenLgWidth / 2.5}px;
       min-width: ${props => props.theme.screenLgWidth / 2.5}px;
       margin-left: auto;
       margin-right: auto;
     }
 
-    @media (max-width: ${props => props.theme.screenLgMin}px) {
-      padding-left: 25px;
-      padding-right: 25px;
-    }
+    padding-left: 25px;
+    padding-right: 25px;
   }
 
   margin-top: 50px;
   flex-direction: column;
-  align-items: center;
+  align-items: stretch;
+  justify-content: stretch;
+  flex: 1;
 `
 
 const FormContainer = styled(Div)`
   flex-direction: column;
-  align-items: center;
+  align-items: stretch;
+  justify-content: stretch;
+  flex: 1;
 `
 const FormHeaderContainer = styled(Div)`
   flex-direction: row;
@@ -66,8 +70,9 @@ const InvisibleCloseQueryButton = styled.a`
 const ProjectQueryForm = styled(Form)`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
+  align-items: stretch;
+  justify-content: stretch;
+  flex: 1;
 `
 
 const Label = styled.label`
@@ -89,7 +94,7 @@ const Text = styled.text`
   margin-bottom: 20px;
 `
 
-const MessageSentTitle = styled(H3)`
+const MessageSentTitle = styled(H5)`
   margin: 40px;
   text-align: center;
 `
@@ -106,7 +111,7 @@ export default class ProjectForm extends Component {
 
   onFormSubmit() {
     if (this.state.name && this.state.email && this.state.message) {
-      const timestamp = moment().tz('Europe/London').utc().format()
+      const timestamp = moment().utc().format()
       sendMessage({
         [timestamp]: {
           userName: this.state.name,
@@ -136,14 +141,14 @@ export default class ProjectForm extends Component {
   render() {
     return (
       <OuterContainer>
+        <FormHeaderContainer>
+          <InvisibleCloseQueryButton>X</InvisibleCloseQueryButton>
+          <H3>Tell us about your project</H3>
+          <CloseQueryButton href="#" onClick={() => this.props.onXClick()}>X</CloseQueryButton>
+        </FormHeaderContainer>
         {this.state.messageSent
           ? <MessageSentTitle>Your enquiry has been sent, we will be in touch soon</MessageSentTitle>
           : <FormContainer>
-              <FormHeaderContainer>
-                <InvisibleCloseQueryButton>X</InvisibleCloseQueryButton>
-                <H3>Tell us about your project</H3>
-                <CloseQueryButton href="#" onClick={() => this.props.onXClick()}>X</CloseQueryButton>
-              </FormHeaderContainer>
               <ProjectQueryForm onSubmit={() => this.onFormSubmit()}>
                 <InputContainer>
                   <Label htmlFor="name">Name:</Label>
