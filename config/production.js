8b666ed39fca458e36da6821dfc101d5deaf676d
Richard Gill <richard@rgill.co.uk>
2018-01-24 12:30:31 +0000

    added mixpanel analytics

--- config/production.js
+++ config/production.js
@@ -5,6 +5,7 @@ export default {
     databaseURL: "https://z-dev-site-prod.firebaseio.com",
     projectId: "z-dev-site-prod",
     storageBucket: "z-dev-site-prod.appspot.com",
-    messagingSenderId: "1050565179661"
-  }
+    messagingSenderId: "1050565179661",
+  },
+  mixPanelToken: '54835cf0158a4cb33b3adee30033e350',
 }
